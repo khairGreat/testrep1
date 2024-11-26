7a599dc2cf8732760abb470f88b2266e875990c7
KhairGreat <pama.am956@s.msumain.edu.ph>
2024-11-27 02:23:21 +0800

    fuck

--- main.js
+++ main.js
@@ -3,5 +3,9 @@ let arr = [ 1 ,2 ,3, 4, 5 ]
 
 
 let result = arr.map ( data => data * 2)
-console.log(result);
+let reduced = arr.reduce ( ( accumulated , cur_data ) => {
+    return accumulated  + cur_data 
+});
+
+let filtered = arr.filter ( data => data % 2 == 0);
 
